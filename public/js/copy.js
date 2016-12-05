50a9fe47c45e5f5e68bd76ebf9311064c299ba06
Danny Pernik <dannypernik@gmail.com>
2016-12-05 01:54:51 -0600

    nominate pages, links for ui demo, symbol change

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,4 +1,4 @@
-var client = new ZeroClipboard( document.getElementById("copy-link") );
+var client = new ZeroClipboard( $('#copy-link'));
 
 client.on( "ready", function( readyEvent ) {
   // alert( "ZeroClipboard SWF is ready!" );
@@ -8,6 +8,21 @@ client.on( "ready", function( readyEvent ) {
     // `event.target` === the element that was clicked
     //event.target.style.display = "none";
     var button = $("#copy-link");
+  	button.data("text-original", button.text());
+  	button.text(button.data("text-swap")).blur();
+  } );
+} );
+
+var clip = new ZeroClipboard( $('#copy-text'));
+
+clip.on( "ready", function( readyEvent ) {
+  // alert( "ZeroClipboard SWF is ready!" );
+
+  clip.on( "aftercopy", function( event ) {
+    // `this` === `client`
+    // `event.target` === the element that was clicked
+    //event.target.style.display = "none";
+    var button = $("#copy-text");
   	button.data("text-original", button.text());
   	button.text(button.data("text-swap")).blur();
   } );
